9d76a687cf64d78c6fa73c5602e7fc19aa68418e
Jai Juneja <jaijuneja@Sahils-Mac-Studio.local>
2025-09-28 13:07:25 +0530

    Initial commit after re-initializing Git

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main>
+      <h1>Reserve a Table</h1>
+      <p>Welcome to Little Lemon! Book your table now.</p>
+      {/* You can later add the form here */}
+    </main>
+  );
+}
+
+export default Main;
