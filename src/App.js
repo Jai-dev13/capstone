8d7cd7eda8dbd31bef5d418458a1448859cf4a8e
Jai Juneja <jaijuneja@Sahils-Mac-Studio.local>
2025-09-28 13:24:59 +0530

    modified CSS styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
+import './App.css';  
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Header />
       <Nav />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 }
 
